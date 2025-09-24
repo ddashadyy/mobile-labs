cb72d37f8434a16087494268ce561c14d695e78a
ddashadyy <poletaevgena75@gmail.com>
2025-09-24 19:29:33 +0800

    game preset

---
+++ comparable-characters/src/com/gennadiy/Game.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import com.gennadiy.AttributeType;
+import com.gennadiy.Character;
+import com.gennadiy.EmberSpirit;
+import com.gennadiy.Huskar;
+import com.gennadiy.Invoker;
+
+
+public class Game {
+
+    public static void main(String[] args) {
+        
+        List<Character> characters = new ArrayList<>();
+
+        characters.add(new Invoker("Invoker", 540, 411, "Invoke", AttributeType.INTELLEGENCE));
+    
+
+
+    }
+
+
+
+}
