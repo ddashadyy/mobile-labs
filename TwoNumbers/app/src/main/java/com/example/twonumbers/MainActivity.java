3c172759ca172705b1e42986c6236249b187b3be
ddashadyy <poletaevgena75@gmail.com>
2025-10-16 16:06:48 +0800

    add import

--- TwoNumbers/app/src/main/java/com/example/twonumbers/MainActivity.java
+++ TwoNumbers/app/src/main/java/com/example/twonumbers/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText et1, et2;
